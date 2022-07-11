6659fe029d3d09e7ed1a5dffa99b386ad1f7543d
Inhoob <inhoob@gmail.com>
2022-07-11 22:17:20 +0900

    onToggle 구현

--- Chap10.일정관리 웹 어플리케이션 만들기/todo-app/src/App.js
+++ Chap10.일정관리 웹 어플리케이션 만들기/todo-app/src/App.js
@@ -2,12 +2,58 @@ import React from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
-
+import { useState, useRef, useCallback } from 'react';
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정관리 앱 만들기',
+      checked: false,
+    },
+  ]);
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+  const nextId = useRef(4); // useState를 안쓴 이유 : id는 어차피 렌더링되는 정보가 아니기 때문에
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
   return (
     <TodoTemplate>
-      <TodoInsert>app을만들자</TodoInsert>
-      <TodoList></TodoList>
+      <TodoInsert onInsert={onInsert}></TodoInsert>
+      <TodoList todos={todos} onRemove={onRemove}></TodoList>
     </TodoTemplate>
   );
 };
